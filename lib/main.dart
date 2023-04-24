8da430a752f6ffc20b61eb72b1486fa9e6f1a442
andre-alcas <alcas.andre@gmail.com>
2023-04-24 11:09:24 -0300

    app alcool gasolina

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
-import 'package:alcool_gasolina/campo_texto.dart';
+import 'package:alcool_gasolina/Home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const CampoTexto(),
-    );
-  }
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
